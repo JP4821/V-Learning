25010f83a18a1bb70aeed80309ac28021ed90ad8
Pjh48 <pjh01102017@gmail.com>
2022-07-21 17:09:50 +0530

    mobile herosection updated

--- src/Components/Herosection.js
+++ src/Components/Herosection.js
@@ -7,14 +7,14 @@ import Slide2 from '../Assets/slide2.png'
 const Herosection = () => {
     return (
         <section>
-            <Carousel fade pause>
+            <Carousel fade pause className="slider">
                 <Carousel.Item>
                     <img src={Slide} alt="First Slider" className="img-fluid" />
                     <Carousel.Caption className="text-start translate-middle">
-                        <h5>HOW TO BECOME A</h5>
+                        <h5 className="h5">HOW TO BECOME A</h5>
                         <h3>FULL STACK WEB DEVELOPER?</h3>
                         <p>Start your new career as a Full Stack Web Developer</p>
-                        <Button variant="warning" className="px-5">Register Now</Button>
+                        <Button variant="warning" className="px-5 px-sm-2">Register Now</Button>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -24,20 +24,20 @@ const Herosection = () => {
                         <h3>Full Stack Web Developer - MERN Stack</h3>
                         <p>Our Full Stack Developer course will allow you to learn how to create full-stack <br/>web 
                         applications using the front-end and back-end JavaScript technologies.</p>
-                        <Button variant="warning" className="px-5">Register Now</Button>
+                        <Button variant="warning" className="px-5 px-sm-2">Register Now</Button>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img src={Slide2} alt="Second Slider" className="img-fluid" />
                     <Carousel.Caption className="text-start translate-middle">
-                        <h4>Become a Full Stack Developer with V Learning Program</h4>
+                        <h3>Become a Full Stack Developer with V Learning Program</h3>
                         <ul>
                             <li>Upskill in DSA & System Design with specialization in Full Stack Development</li>
                             <li>Build real-world projects by mastering React, MERN, JavaScript, etc.</li>
                             <li>Learn the essential Full Stack development concepts from scratch</li>
                         </ul>
-                        <Button variant="warning" className="px-5">Register Now</Button>
-                        <Button variant="info" className="px-5 ms-2">Download Syllabus</Button>
+                        <Button variant="warning" className="px-5 px-sm-2">Register Now</Button>
+                        <Button variant="info" className="px-5 ms-2 px-sm-2">Download Syllabus</Button>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
